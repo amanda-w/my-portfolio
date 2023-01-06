8cf63b6f14de95038e19eef7610553d70b253354
amanda-w <amandanwong@gmail.com>
2023-01-05 20:46:51 -0800

    add deployment links for projects

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -12,10 +12,12 @@ function Project() {
                 <img src={SpaceSnake} alt="" />
                 <p>Space Snake Game</p>
                 <a href="https://github.com/amanda-w/Space-Snake">Github</a>
+                <a href="https://amanda-w.github.io/Space-Snake/">Check it out!</a>
                 <h3>Project 2</h3>
                 <img src={Recipe} alt="" />
                 <p>Recipe Search</p>
                 <a href="https://github.com/amanda-w/Recipes">Github</a>
+                <a href="https://search-fave-recipes-app.herokuapp.com/">Check it out!</a>
         </div>
     )
 }
